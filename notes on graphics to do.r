ceaab17137f21c3c349f2a870c6ee8e7810a13d3
ejanalyst <info@ejanalysis.com>
2015-02-09 01:04:02 -0500

    remove extra files from folder

--- notes on graphics to do.r
+++
@@ -1,7 +0,0 @@
-
-GRAPHICS:
-
-stripchart of 1 variable ~ zone, for each var's regionpctile
-unwtd and wtd: boxplot of 1 variable ~ zone, for each var's regionpctile
-unwtd and wtd: histo of 1 variable with horizontal line at expected. for each var's uspctile
-heatmap
